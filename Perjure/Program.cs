979e188265ea3d3989991e10a52175b86e07eee4
Connor O'Shea <connor@connoroshea.com>
2016-05-14 10:39:07 -0400

    Initial commit

---
+++ Perjure/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Perjure {
+    public class Program {
+        public static void Main(string[] args) {
+            var startTime = DateTime.UtcNow;
+            Console.WriteLine($"Program started at {startTime.ToString("MM/dd/yyyy hh:mm:ss tt")} UTC");
+
+            var settingsFilePath = GetSettingsFilePath(args);
+            Console.WriteLine($"Settings file: {settingsFilePath}");
+
+            var rules = ReadRulesFromSettingsFile(settingsFilePath);
+
+            Console.WriteLine("Processing rules...\n");
+            ProcessRules(rules);
+
+            var endTime = DateTime.UtcNow;
+            Console.WriteLine($"\nProgram ended at {endTime.ToString("MM/dd/yyyy hh:mm:ss tt")} UTC");
+            Console.WriteLine($"Total seconds elapsed: {(endTime - startTime).TotalSeconds.ToString("N")}");
+
+            Console.ReadLine();
+        }
+
+        private static string GetSettingsFilePath(string[] args) {
+            return args.Length == 1
+                    ? args[0]
+                    : Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\Settings.json";
+        }
+
+        private static List<PurgeRule> ReadRulesFromSettingsFile(string settingsFilePath) {
+            return JsonConvert.DeserializeObject<List<PurgeRule>>(File.ReadAllText(settingsFilePath));
+        }
+
+        private static void ProcessRules(List<PurgeRule> rules) {
+            foreach (var rule in rules) {
+                rule.Process();
+            }
+        }
+    }
+}
---
+++ Perjure/PurgeRule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Perjure {
+    public class PurgeRule {
+        public string DirectoryPath { get; set; }
+        public string MatchPattern { get; set; }
+        public short DaysToPurgeAfter { get; set; }
+        public bool IncludeSubfolders { get; set; }
+        public bool IncludeHiddenFiles { get; set; }
+
+        internal void Process() {
+            Console.WriteLine(DirectoryPath);
+            for (int i = 0; i < DirectoryPath.Length; i++) {
+                Console.Write("=");
+            }
+            Console.WriteLine();
+
+            var directory = new DirectoryInfo(DirectoryPath);
+
+            var allFiles = directory.GetFiles("*", IncludeSubfolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+
+            var regexPattern = new Regex(MatchPattern);
+
+            var filesToPurge = allFiles.Where(f => !f.Attributes.HasFlag(FileAttributes.System) &&
+                                                   regexPattern.IsMatch(f.Name) &&
+                                                   (DateTime.UtcNow - f.CreationTimeUtc).Days > DaysToPurgeAfter);
+
+            if (!IncludeHiddenFiles) {
+                filesToPurge = filesToPurge.Where(f => !f.Attributes.HasFlag(FileAttributes.Hidden));
+            }
+
+            foreach (var file in filesToPurge.ToList()) {
+                file.Delete();
+                Console.WriteLine($"Deleted {file.FullName}");
+            }
+        }
+    }
+}
