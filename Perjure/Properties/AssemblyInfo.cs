d8651c4a9bb6e05abdcbcc6c6c0f2f3748fc1670
Connor O'Shea <connor@connoroshea.com>
2016-08-21 14:10:34 -0400

    Added icon

--- Perjure/Properties/AssemblyInfo.cs
+++ Perjure/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Perjure")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Purge folders after the number of days you define")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Perjure")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Connor O'Shea")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
