87bff607b09148ec6a6634949f867565dd56e36b
Connor O'Shea <connor@connoroshea.com>
2019-07-21 16:42:33 -0400

    Add option to clean all users' temp folders (close #7)

--- Perjure/Configuration.cs
+++ Perjure/Configuration.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using NLog;
 using Perjure.PurgeRules;
 // ReSharper disable MemberCanBePrivate.Global
 // ReSharper disable UnusedAutoPropertyAccessor.Global
@@ -7,7 +12,10 @@ namespace Perjure
 {
     public class Configuration
     {
+        private static readonly ILogger Log = LogManager.GetCurrentClassLogger();
+
         public bool IncludeRecycleBin { get; set; }
+        public bool IncludeTempDirectories { get; set; }
         public InternetExplorerPurgeOptions InternetExplorerPurgeOptions { get; set; }
         public List<FilePurgeRule> FilePurgeRules { get; set; }
 
@@ -23,6 +31,11 @@ namespace Perjure
                 yield return new InternetExplorerPurgeRule(InternetExplorerPurgeOptions);
             }
 
+            if (IncludeTempDirectories)
+            {
+                FilePurgeRules.AddRange(BuildTempDirectoryFilePurgeRules());
+            }
+
             if (FilePurgeRules != null)
             {
                 foreach (var purgeRule in FilePurgeRules)
@@ -30,6 +43,41 @@ namespace Perjure
                     yield return purgeRule;
                 }
             }
+        }
+
+        private IEnumerable<FilePurgeRule> BuildTempDirectoryFilePurgeRules()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Log.Warn("Purging temporary directories is only supported on Windows.");
+                yield break;
+            }
+
+            yield return BuildTempDirectoryFilePurgeRule(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Windows), "Temp"));
+
+            var usersDirectory = Path.GetFullPath(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".."));
+            var userTempDirectories = Directory.EnumerateDirectories(usersDirectory)
+                .Select(d => Path.Combine(d, "AppData", "Local", "Temp"))
+                .Where(Directory.Exists);
+
+            foreach (var userTempDirectory in userTempDirectories)
+            {
+                yield return BuildTempDirectoryFilePurgeRule(userTempDirectory);
+            }
+        }
+
+        private static FilePurgeRule BuildTempDirectoryFilePurgeRule(string tempDirectory)
+        {
+            return new FilePurgeRule
+            {
+                DirectoryPath = tempDirectory,
+                DaysToPurgeAfter = 7,
+                IncludeSubdirectories = true,
+                IncludeHiddenFiles = false,
+                DeleteEmptySubdirectories = true,
+                DaysToPurgeEmptySubdirectoriesAfter = 7,
+                TimeComparison = TimeComparison.LastAccess
+            };
         }
     }
 }
